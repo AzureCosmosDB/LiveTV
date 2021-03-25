7a283ef6784c13f05fb58c0e301d397007c52167
Mark Brown <mjbrown@microsoft.com>
2021-03-25 13:33:27 -0700

    here's the code

--- ep3-simple-net-sdk/Program.cs
+++ ep3-simple-net-sdk/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Microsoft.Azure.Cosmos;
 
 
 namespace episode3
@@ -10,12 +11,21 @@ namespace episode3
     class Program
     {
 
+        static string uri = "https://localhost:8081";
+        static string key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        static string databaseId = "database1";
+        static string containerId = "families";
+
+        static Container container;
+        static CosmosClient client;
 
 
         static async Task Main()
         {
 
-
+            client = new CosmosClient(uri, key);
+            Database db = await client.CreateDatabaseIfNotExistsAsync(databaseId);
+            container = await db.CreateContainerIfNotExistsAsync(containerId, "/lastName", 400);
 
 
             Family family = await Create();
@@ -30,38 +40,107 @@ namespace episode3
 
         public static async Task<Family> Create()
         {
-            Family family;
+            Family family = new Family()
+            {
+                Id = Guid.NewGuid().ToString(),
+                FirstName = "Mark",
+                LastName = "Brown",
+                Age = 30
+            };
 
+            Response<Family> response = await container.CreateItemAsync<Family>(
+                family,
+                new PartitionKey(family.LastName));
+
+            Print(response.Resource);
+
+            Console.WriteLine(response.RequestCharge);
 
             return family;
         }
 
         public static async Task<List<Family>> Query(string lastName, string id)
         {
-            List<Family> family = new();
+            List<Family> families = new();
+
+            string sql = "select * from c where c.id = @id";
+
+            FeedIterator<Family> resultSet = container.GetItemQueryIterator<Family>(
+                new QueryDefinition(sql)
+                .WithParameter("@id", id),
+                requestOptions: new QueryRequestOptions()
+                {
+                    PartitionKey = new PartitionKey(lastName)
+                });
+
+            while (resultSet.HasMoreResults)
+            {
+                FeedResponse<Family> response = await resultSet.ReadNextAsync();
+
+                foreach(Family family in response)
+                {
+                    families.Add(family);
+                }
 
 
-            return family;
+                Console.WriteLine(response.RequestCharge);
+            }
+
+            return families;
 
         }
 
         public static async Task<Family> Get(string lastName, string id)
         {
-            
+            Response<Family> response = await container.ReadItemAsync<Family>(
+                id: id,
+                partitionKey: new PartitionKey(lastName));
+
+            Console.WriteLine(response.RequestCharge);
+
+            return response.Resource;            
         }
 
         public static async Task Update(Family family)
         {
 
+            await container.ReplaceItemAsync<Family>(
+                family,
+                id: family.Id,
+                partitionKey: new PartitionKey(family.LastName)
+                );
         }
 
         public static async Task Delete(string lastName, string id)
         {
-
+            await container.DeleteItemAsync<Family>(id, new PartitionKey(lastName));
         }
 
         public static async Task FamilyTransaction()
         {
+            Family family1 = new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                FirstName = "David",
+                LastName = "Brown",
+                Age = 25
+            };
+
+            Family family2 = new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                FirstName = "Mary",
+                LastName = "Brown",
+                Age = 27
+            };
+
+            TransactionalBatchResponse batch = await container.CreateTransactionalBatch(
+                new PartitionKey("Brown"))
+                .CreateItem<Family>(family1)
+                .CreateItem<Family>(family2)
+                .ExecuteAsync();
+
+            Console.WriteLine(batch.RequestCharge);
 
         }
 
