eb78019cbe19ef78923ab73f02d499f1e85ea6c6
Mark Brown <mjbrown@microsoft.com>
2021-03-24 20:49:21 -0700

    initial commit

---
+++ ep3-simple-net-sdk/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace episode3
+{
+    class Program
+    {
+
+
+
+        static async Task Main()
+        {
+
+
+
+
+            Family family = await Create();
+            List<Family> familyList = await Query(family.LastName, family.Id);
+            family = await Get(family.LastName, family.Id);
+            family.Age = 40;
+            await Update(family);
+            await FamilyTransaction();
+            await Delete(family.LastName, family.Id);
+
+        }
+
+        public static async Task<Family> Create()
+        {
+            Family family;
+
+
+            return family;
+        }
+
+        public static async Task<List<Family>> Query(string lastName, string id)
+        {
+            List<Family> family = new();
+
+
+            return family;
+
+        }
+
+        public static async Task<Family> Get(string lastName, string id)
+        {
+            
+        }
+
+        public static async Task Update(Family family)
+        {
+
+        }
+
+        public static async Task Delete(string lastName, string id)
+        {
+
+        }
+
+        public static async Task FamilyTransaction()
+        {
+
+        }
+
+        public static void Print(object obj)
+        {
+            Console.WriteLine($"{JObject.FromObject(obj)}\n");
+        }
+    }
+
+
+    class Family
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty(PropertyName = "lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty(PropertyName = "age")]
+        public int Age { get; set; }
+    }
+}
